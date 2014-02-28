0d3d1008ddeead74e1ef05f7e714de6c31e2c072
Apprentice <apprentice@dbc06.local>
2014-02-28 15:43:47 -0800

    Removed some methods from controller and updated.

--- controller.rb
+++ controller.rb
@@ -1,12 +1,12 @@
 # controller class
 require_relative 'view.rb'
+require_relative 'model.rb'
+
+name = ARGV[0]
 
 class controller
 
-  def initialize(*player, length_of_board)
-    @player_name = player
-    @players = [] # array of the number of players all initalized at starting position 0.
-  end
+  our_game = FlashcardGame.new(name, 10)
 
   def move_player!(player, absolute_position)
     # player is the current player
@@ -14,41 +14,28 @@ class controller
     player[player-1] = absolute_position
   end
 
-  def select_card
-    # Pull card from deck and pass
-  end
-
-  def verify_answer(player_answer, actual)
-    # check and deliver answer
-    # If correct, tell the player they got it right, and:
-    # possibly move the player here or in game method
-    # If they got it wrong, tell them they gave wrong answer and provide the correct one
-    # possibly move player here of in game method
-  end
-
-  def finished?
-    # Checks to see if any of the players location is == length of the board
-  end
-
   def game
 
+    game_done = false
     # Loop through until the game is finished, pulling a new card each loop for each player
-    current_player = 0
-    until game.finished?
+    #current_player = 0
+    until game_done
 
 
       print_board(racer_positions, length)
-      current_card = select_card
+      #current_card = select_card
+      current_card = our_game.get_next_flashcard
 
-      print_question(cur_player, current_card.question)
-      answer = gets.chomp
-      verify_answer(answer, current_card.answer)
+      print_question(our_game.player.name, current_card.question)
+      guess = gets.chomp
+      current_card.correct?(guess)
 
       # Dealing with ties at a later time?
-      current_player += 1
-      if current_player > num_of_players
-        current_player = 0
-      end
+      # current_player += 1
+      # if current_player > num_of_players
+      #   current_player = 0
+      # end
+      game_done = our_game.finished?
     end
 
   end
